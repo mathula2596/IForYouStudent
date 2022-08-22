bec401f30563599561aa00fc1dbb912cfc29cb95
Mathula <mathula2504@gmail.com>
2022-08-22 15:55:32 +0100

    Login and Menu

--- app/src/main/java/com/ifu/iforyoustudent/StudentDashboard.java
+++ app/src/main/java/com/ifu/iforyoustudent/StudentDashboard.java
@@ -1,14 +1,85 @@
 package com.ifu.iforyoustudent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class StudentDashboard extends AppCompatActivity {
 
+    private MaterialToolbar appbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private SharedPreferences sharedPreferences;
+    private static final String SHARED_PRE_NAME = "myPreference";
+    private static final String KEY_NAME = "username";
+    private static final String KEY_ROLE = "userType";
+    String loginName, loginRole = null;
+    private View navHeader;
+    public TextView username;
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_dashboard);
+        sharedPreferences = getSharedPreferences(SHARED_PRE_NAME,MODE_PRIVATE);
+        loginName = sharedPreferences.getString(KEY_NAME,null);
+        loginRole = sharedPreferences.getString(KEY_ROLE,null);
+
+        navigationView =findViewById(R.id.navigation_view);
+
+        navHeader = navigationView.findViewById(R.layout.nav_header);
+
+        appbar= findViewById(R.id.appbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        appbar.setNavigationOnClickListener(view -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+            username = navigationView.findViewById(R.id.username);
+            username.setText(loginName);
+        });
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id)
+                {
+                    case R.id.nav_logout:
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.clear();
+                        editor.commit();
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        finish();
+                        break;
+                    case R.id.nav_home:
+                        Toast.makeText(getApplicationContext(), "Settings is Clicked",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        return true;
+
+                }
+                return true;
+            }
+        });
+
     }
 }
